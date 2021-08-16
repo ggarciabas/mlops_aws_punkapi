18bfc467927566e31538d61f0129f57c1da1d67b
Giovanna G B Fritsche <ggarciabas@gmail.com>
2021-08-16 17:51:40 -0300

    adiciona codigo para lambda

---
+++ sam-aws/punkapi_clean/app.py
@@ -0,0 +1,24 @@
+import base64
+
+def lambda_handler(event, context):
+    output = []
+
+    print (f"Leitura dos registros: {len(event['records'])}"):
+    for record in event['records']:
+        print(record['recordId'])
+        payload = base64.b64decode(record['data'])
+
+        # Do custom processing on the payload here
+
+        output_record = {
+            'recordId': record['recordId'],
+            'result': 'Ok',
+            'data': base64.b64encode(payload)
+        }
+        output.append(output_record)
+
+    print('Processado {} registros com sucesso.'.format(len(event['records'])))
+
+    return {'records': output}
+
+# Ref: https://github.com/amazon-archives/serverless-app-examples/blob/master/python/kinesis-firehose-process-record-python/lambda_function.py
